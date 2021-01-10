d78efd577a80628e2063866ea115b0ffd169838f
faysalkarasoy <77213914+faysalkarasoy@users.noreply.github.com>
2021-01-10 03:09:50 +0300

    Update Ornek.vb

--- Ornek.vb
+++ Ornek.vb
@@ -1,16 +1,20 @@
 Imports System.Threading
 
-Public Sub MailGonder()
- Dim EM As New EmailSinifi
- Dim TH As New System.Threading.Thread(AddressOf EM.SendMail) With {.IsBackground = True}
- EM.GondericiAdi = "Sozluk Yazılımı"
- EM.GondericiAdresi = "iletisim@sozlukyazilimi.com"
- EM.GondericiSifresi = "mail şifresi"
- EM.SMTP_Sunucu = "smtp.yandex.com"
- EM.SMTP_Portu = 587
- EM.EnableSsl = True
- EM.MailBaslik = "Deneme Başlık"
- EM.MailIcerigi = "Deneme İçerik"
- EM.MailAliciAdres = "alici@mailadres.com"
- TH.Start()
-End Sub
+Public Class Ornek
+ 
+ Public Sub MailGonder()
+  Dim EM As New EmailSinifi
+  Dim TH As New System.Threading.Thread(AddressOf EM.SendMail) With {.IsBackground = True}
+  EM.GondericiAdi = "Sozluk Yazılımı"
+  EM.GondericiAdresi = "iletisim@sozlukyazilimi.com"
+  EM.GondericiSifresi = "mail şifresi"
+  EM.SMTP_Sunucu = "smtp.yandex.com"
+  EM.SMTP_Portu = 587
+  EM.EnableSsl = True
+  EM.MailBaslik = "Deneme Başlık"
+  EM.MailIcerigi = "Deneme İçerik"
+  EM.MailAliciAdres = "alici@mailadres.com"
+  TH.Start()
+ End Sub
+ 
+End Class
